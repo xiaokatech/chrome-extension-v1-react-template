e7cc07a54a3836b4fdf761365305227ec1531e1d
Jiean Yang <jiean.yang.freelancer@gmail.com>
2025-07-06 16:41:42 +0800

    Set path alias for magicUI

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import path from "path";
+
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
@@ -23,4 +25,9 @@ export default defineConfig({
       manifest: generateManifest,
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
