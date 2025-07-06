36978cc5188100b0225edbb6249e1aa3a78e9cf5
Jiean Yang <jiean.yang.freelancer@gmail.com>
2025-07-06 12:48:41 +0800

    Create src/enums/EContextMenuItem.ts

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,5 @@
 import browser from "webextension-polyfill";
+import { EContextMenuItem } from "./enums/EContextMenuItem";
 
 console.log("Hello from the background!");
 
@@ -12,13 +13,13 @@ browser.runtime.onInstalled.addListener((details) => {
 
 function setupContextMenu() {
   chrome.contextMenus.create({
-    id: "select_text",
+    id: EContextMenuItem.SELECT_TEXT,
     title: "Select Text",
     contexts: ["selection"],
   });
 
   chrome.contextMenus.create({
-    id: "openSidePanel",
+    id: EContextMenuItem.OPEN_SIDE_PANEL,
     title: "Open side panel",
     contexts: ["all"],
   });
@@ -36,7 +37,7 @@ chrome.contextMenus.onClicked.addListener((data, tab) => {
 
   console.log("data in contextMenus.onClicked:", data);
 
-  if (data.menuItemId === "select_text") {
+  if (data.menuItemId === EContextMenuItem.SELECT_TEXT) {
     // Store the last word in chrome.storage.session.
     chrome.storage.session.set({ selectedText: data.selectionText });
 
@@ -44,7 +45,7 @@ chrome.contextMenus.onClicked.addListener((data, tab) => {
     chrome.sidePanel.open({ tabId: tab.id });
   }
 
-  if (data.menuItemId === "openSidePanel") {
+  if (data.menuItemId === EContextMenuItem.OPEN_SIDE_PANEL) {
     // This will open the panel in all the pages on the current window.
     chrome.sidePanel.open({ windowId: tab.windowId });
   }
---
+++ src/enums/EContextMenuItem.ts
@@ -0,0 +1,4 @@
+export enum EContextMenuItem {
+  SELECT_TEXT = "select_text",
+  OPEN_SIDE_PANEL = "openSidePanel",
+}
